557f77d559ac2ad965d32890befa0d4e3815cd1a
Simon-Otalvaro <Simonotalvaro6@gmail.com>
2025-07-16 17:01:54 -0500

    Actualizacion front, en footer y seccion agregar

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,30 +1,33 @@
-import React from 'react'
-import './Footer.css'
-import logo_b from '../../assets/logo-blanco.png'
-import whatsapp from '../../assets/whatsapp.png'
-import facebook from '../../assets/facebook.png'
-import instagram from '../../assets/instagram.png'
+import React from 'react';
+import './Footer.css';
+import logo_b from '../../assets/logo-blanco.png';
+import whatsapp from '../../assets/whatsapp.png';
+import facebook from '../../assets/facebook.png'; // <- imagen, no URL
+import instagram from '../../assets/instagram.png';
 
 export const Footer = () => {
   return (
     <div className='Footer'>
-        <div className='footer'>
+      <div className='footer'>
         <img className='img-f' src={logo_b} alt="logo" />
-      <div className='direction'>
-        <p>Dirección: Carrera 24 No. 39-54, Calarcá, Quindío</p>
-        <br />
-        <p>Direccion: Carrera 16 No. 44-210 barrio los Tanques, Calarcá</p>
-        <br />
-        <p>Email: contactenos@emca-calarca-quindio.gov.co</p>
-      </div>
 
-      <div className='icons'>
-        <img className='icon' src={whatsapp} alt="whatsapp" />
-        <img className='icon' src={facebook} alt="facebook" />
-        <img className='icon' src={instagram} alt="instagram" />
-      </div>
-          </div> 
+        <div className='direction'>
+          <p>Dirección: Carrera 24 No. 39-54, Calarcá, Quindío</p>
+          <br />
+          <p>Dirección: Carrera 16 No. 44-210 barrio los Tanques, Calarcá</p>
+          <br />
+          <p>Email: contactenos@emca-calarca-quindio.gov.co</p>
+        </div>
 
+        <div className='icons'>
+          <a href="https://www.facebook.com/emcaesp" target="_blank" rel="noopener noreferrer">
+            <img className='icon' src={facebook} alt="facebook" />
+          </a>
+          <a href="https://www.instagram.com/emcaesp/" target="_blank" rel="noopener noreferrer">
+            <img className='icon' src={instagram} alt="instagram" />
+          </a>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
